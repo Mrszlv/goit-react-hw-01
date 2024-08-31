d3925dbbfe28d0481e87e6d62aabdecc565db242
Mrszlv <miroszlav.popovics@gmail.com>
2024-08-31 15:25:55 +0300

    commit-table-08

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import userData from './userData.json';
 import Profile from './components/Profile/Profile';
 import FriendList from './components/FriendList/FriendList';
-import friends from './friends.json'
+import friends from './friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './transactions.json';
 const App = () => {
   return (
     <div>
+      
       <Profile 
       name={userData.username}
       tag={userData.tag}
@@ -14,6 +17,9 @@ const App = () => {
       />
 
       <FriendList friends={friends}/>
+
+      <TransactionHistory items={transactions}/>
+
     </div>
   );
 }
